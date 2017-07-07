766d995a2318bee3a8cdf183d58a8172c2138f64
mayan <mayan@oradt.com>
2017-07-07 17:59:16 +0800

    output configuration

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,19 +1,123 @@
 #include <jni.h>
 #include <string>
+#include <sstream>
+#include <iomanip>
 
-extern "C"
-{
+extern "C" {
 #include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
+#include "libavformat/avformat.h"
 #include <libavfilter/avfilter.h>
+
+#ifdef ANDROID
+#include <android/log.h>
+#define LOGE(format, ...) __android_log_print(ANDROID_LOG_ERROR, "FFmpeg", format, ##__VA_ARGS__)
+#else
+#define LOGE(format, ...) (void)(0)
+#endif
+
+struct URLProtocol;
+JNIEXPORT jstring JNICALL
+Java_com_furzoom_lab_ffmpeg_MainActivity_avformatinfo(JNIEnv *env, jobject instance) {
+    std::stringstream ss;
+    av_register_all();
+    AVInputFormat *if_temp = av_iformat_next(NULL);
+    AVOutputFormat *of_temp = av_oformat_next(NULL);
+
+    // Input
+    while (if_temp) {
+        ss << "[In ][" << std::setw(23) << if_temp->name << "]\n";
+        if_temp = if_temp->next;
+    }
+    ss << "\n";
+
+    // Output
+    while (of_temp) {
+        ss << "[Out][" << std::setw(23) << of_temp->name << "]\n";
+        of_temp = of_temp->next;
+    }
+    return env->NewStringUTF(ss.str().c_str());
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_furzoom_lab_ffmpeg_MainActivity_avcodecinfo(JNIEnv *env, jobject instance) {
+    std::stringstream ss;
+    av_register_all();
+    AVCodec *codec = av_codec_next(NULL);
+
+    while (codec) {
+        if (codec->decode) {
+            ss << "[Dec]";
+        } else {
+            ss << "[Enc]";
+        }
+
+        switch (codec->type) {
+            case AVMEDIA_TYPE_VIDEO:
+                ss << "[Video]";
+                break;
+            case AVMEDIA_TYPE_AUDIO:
+                ss << "[Audio]";
+                break;
+            default:
+                ss << "[Other]";
+                break;
+        }
+        ss << "[" << std::setw(17) << codec->name << "]\n";
+        codec = codec->next;
+    }
+    return env->NewStringUTF(ss.str().c_str());
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_furzoom_lab_ffmpeg_MainActivity_avfilterinfo(JNIEnv *env, jobject instance) {
+    std::stringstream ss;
+    avfilter_register_all();
+    LOGE("%s", "in native avfilterinfo");
+    const AVFilter *filter = avfilter_next(NULL);
+    while (filter) {
+        ss << "[" << std::setw(17) << filter->name << "]\n";
+        LOGE("%s", filter->name);
+        filter = filter->next;
+    }
+    LOGE("%s", ss.str().c_str());
+    return env->NewStringUTF(ss.str().c_str());
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_furzoom_lab_ffmpeg_MainActivity_configure(JNIEnv *env, jobject instance) {
+    av_register_all();
+    std::string info(avcodec_configuration());
+    return env->NewStringUTF(info.c_str());
+}
 
 JNIEXPORT jstring JNICALL
 Java_com_furzoom_lab_ffmpeg_MainActivity_urlprotocolinfo(JNIEnv *env, jobject instance) {
+    std::stringstream ss;
+    const char *p = NULL;
     av_register_all();
-    char info[1000] = {0};
-    sprintf(info, "%s\n", avcodec_configuration());
+    struct URLProtocol *pup = NULL;
 
-    return env->NewStringUTF(info);
+    // Input
+    struct URLProtocol **p_temp = &pup;
+    p = avio_enum_protocols((void **)p_temp, 0);
+    while ((*p_temp) != NULL) {
+        if (p) {
+            ss << "[In ][" << std::setw(10) << p << "]\n";
+        }
+        p = avio_enum_protocols((void**)p_temp, 0);
+    }
+    ss << "\n";
+    // Output
+    pup = NULL;
+    p = avio_enum_protocols((void**)p_temp, 1);
+    while ((*p_temp) != NULL) {
+        if (p) {
+            ss << "[Out][" << std::setw(10) << p << "]\n";
+        }
+        p = avio_enum_protocols((void**)p_temp, 1);
+    }
+
+    return env->NewStringUTF(ss.str().c_str());
 }
 
 JNIEXPORT jstring JNICALL
@@ -23,6 +127,5 @@ Java_com_furzoom_lab_ffmpeg_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
-
 
 }
--- app/src/main/java/com/furzoom/lab/ffmpeg/MainActivity.java
+++ app/src/main/java/com/furzoom/lab/ffmpeg/MainActivity.java
@@ -2,12 +2,13 @@ package com.furzoom.lab.ffmpeg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button protocol;
+    private static final String TAG = "MainActivity";
     private TextView tv;
 
     @Override
@@ -15,8 +16,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        protocol = (Button) findViewById(R.id.btn_protocol);
-        protocol.setOnClickListener(this);
+        Button btnConfigure = (Button) findViewById(R.id.btn_configure);
+        Button btnProtocol = (Button) findViewById(R.id.btn_protocol);
+        Button btnFormat = (Button) findViewById(R.id.btn_format);
+        Button btnCodec = (Button) findViewById(R.id.btn_codec);
+        Button btnFilter = (Button) findViewById(R.id.btn_filter);
+
+        btnConfigure.setOnClickListener(this);
+        btnProtocol.setOnClickListener(this);
+        btnFormat.setOnClickListener(this);
+        btnCodec.setOnClickListener(this);
+        btnFilter.setOnClickListener(this);
+
         tv = (TextView) findViewById(R.id.sample_text);
     }
 
@@ -26,7 +37,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     public native String stringFromJNI();
 
+    public native String configure();
     public native String urlprotocolinfo();
+    public native String avformatinfo();
+    public native String avcodecinfo();
+    public native String avfilterinfo();
+
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
@@ -35,8 +51,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.btn_configure:
+                tv.setText(configure());
+                break;
             case R.id.btn_protocol:
                 tv.setText(urlprotocolinfo());
+                break;
+            case R.id.btn_format:
+                tv.setText(avformatinfo());
+                break;
+            case R.id.btn_codec:
+                tv.setText(avcodecinfo());
+                break;
+            case R.id.btn_filter:
+                Log.d(TAG, "onClick: btn_filter");
+                tv.setText(avfilterinfo());
+                Log.d(TAG, "After: btn_filter");
                 break;
             default:
                 break;
